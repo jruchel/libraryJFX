be76908abc2bbb7861d68841a45e337a68d2c1dc
jruchel <jruchel254@gmail.com>
2020-12-21 14:11:56 +0100

    some import changes

--- src/web/Requests.java
+++ src/web/Requests.java
@@ -1,10 +1,8 @@
 package web;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
